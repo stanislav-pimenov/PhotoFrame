250cf56b9b1b54e57594ece9a025b968058acec2
Stanislav_Pimenov <stanislav_pimenov@epam.com>
2016-02-14 20:29:20 +0300

    baby branch

--- PhotoWidget/src/main/java/com/esprit/photoframe/photowidget/PhotoWidget.java
+++ PhotoWidget/src/main/java/com/esprit/photoframe/photowidget/PhotoWidget.java
@@ -6,9 +6,11 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.util.Log;
 import android.widget.RemoteViews;
+import net.danlew.android.joda.JodaTimeAndroid;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.joda.time.Days;
+import org.joda.time.Weeks;
 
 import java.util.Arrays;
 
@@ -29,46 +31,29 @@ public class PhotoWidget extends AppWidgetProvider {
         //DateTimeZone MSC = DateTimeZone.forID("Europe/Moscow");
         DateTimeZone UTC = DateTimeZone.forID("UTC");
         DateTime now = new DateTime(UTC);
-        DateTime end = new DateTime(2015, 6, 26, 17, 0, 0, UTC);
-        DateTime benchMark = new DateTime(2015, 6, 26, 0, 0, 0, UTC);
+        DateTime sexDate = new DateTime(2015, 11, 2, 0, 0, 0, UTC);
+        //DateTime benchMark = new DateTime(2015, 6, 26, 0, 0, 0, UTC);
         Resources res = context.getResources();
 
-        Days days = Days.daysBetween(now, end);
+        int weeks = Math.abs(Weeks.weeksBetween(now, sexDate).getWeeks());
+        int days = Math.abs(Days.daysBetween(now, sexDate).getDays()) % 7;
 
-        if (now.isBefore(end)) {
-            String text = String.format(resolveDaysOutputStyle(res, days.getDays(), true), days.getDays());
-            views.setTextViewText(R.id.tv, text);
-        } else {
-            //long daysAfter = Math.abs(days.getDays()) + 1;
-            long daysAfter = Math.abs(Days.daysBetween(now, benchMark).getDays()) + 1;
-            String text = String.format(resolveDaysOutputStyle(res, daysAfter, false), daysAfter);
-            views.setTextViewText(R.id.tv, text);
-        }
-        for (int appWidgetId : appWidgetIds) {
-            appWidgetManager.updateAppWidget(appWidgetId, views);
-        }
+        String text = String.format(resolveDaysOutputStyle(res, days), weeks, days);
+        Log.d(LOG_TAG, "new text is: " + text);
+        views.setTextViewText(R.id.tv, text);
 
+        appWidgetManager.updateAppWidget(appWidgetIds, views);
     }
 
-    private String resolveDaysOutputStyle(Resources res, long days, boolean before) {
-        days = days % 10;
-        if (before) {
-            if (days == 1) {
-                return res.getString(R.string.before1);
-            } else if (days >= 2 && days <= 4) {
-                return res.getString(R.string.before234);
-            } else if (days >= 5 || days == 0) {
-                return res.getString(R.string.before);
-            }
-        } else if (days == 1) {
-            return res.getString(R.string.after1);
+    private String resolveDaysOutputStyle(Resources res, int days) {
+        if (days == 1) {
+            return res.getString(R.string.age1);
         } else if (days >= 2 && days <= 4) {
-            return res.getString(R.string.after234);
+            return res.getString(R.string.age234);
         } else if (days >= 5 || days == 0) {
-            return res.getString(R.string.after);
+            return res.getString(R.string.age);
         }
-
-        return null;
+        return "";
     }
 
     @Override
@@ -86,6 +71,7 @@ public class PhotoWidget extends AppWidgetProvider {
     @Override
     public void onEnabled(Context context) {
         super.onEnabled(context);
+        JodaTimeAndroid.init(context);
         Log.d(LOG_TAG, "onEnabled");
 
     }
